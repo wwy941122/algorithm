f57c38b8be0a9d1d1c27613b74c6a68ccc97d695
wwy941122 <wwy941122@qq.com>
2016-03-14 23:31:54 +0800

    2016.3.14

--- src/binarySearchTree/BianrySearchTree.java
+++ src/binarySearchTree/BianrySearchTree.java
@@ -24,7 +24,9 @@ public class BianrySearchTree {
 			return this.key;
 		}
 		public String toString(){
-			return "("+ leftChild.toString() + ","+key+","+rightChild.toString()+")";
+			 String leftkey = (leftChild == null ? "null" : String.valueOf(leftChild.key));  
+	            String rightkey = (rightChild == null ? "null" : String.valueOf(rightChild.key));  
+	            return "(" + leftkey + " , " + key + " , " + rightkey + ")";  
 		}
 		
 	}
@@ -73,7 +75,7 @@ public class BianrySearchTree {
 	 }*///递归版本
 	 
 	 /**
-	  * minNode返回树的最小节点
+	  * minNode返回树的最小结点
 	 * @throws Exception 
 	  * 
 	  * **/
@@ -89,7 +91,7 @@ public class BianrySearchTree {
 	 }
 	 
 	 /**
-	  * minNode返回树的最大节点
+	  * minNode返回树的最大结点
 	 * @throws Exception 
 	  * 
 	  * **/
@@ -105,6 +107,27 @@ public class BianrySearchTree {
 	 }
 	 
 	 /**
+	  * successor返回一个结点的后继结点
+	 * @throws Exception 
+	  * 
+	  * **/
+	 public TreeNode successor(TreeNode node) throws Exception{
+		 if(node == null){
+			 return null;
+		 }
+		 
+		 if(node.rightChild != null){			//如果结点的右字数不为空，则后继结点是右子树的最左结点
+			 return minNode(node.rightChild);
+		 }
+		 TreeNode parentNode = node.parent;
+		 while(parentNode != null && node == parentNode.rightChild){//若结点的右子树为空，后继结点是x的最底层祖先
+			 node = parentNode;
+			 parentNode = parentNode.parent;
+		 }
+		 return parentNode;
+	 }
+	 
+	 /**
 	  * inOrderTraverse 对给定树中序遍历
 	  * 
 	  * **/
@@ -116,6 +139,106 @@ public class BianrySearchTree {
 			 inOrderTraverse(pNode.rightChild);
 		 }
 	 }
+	 
+	 /**
+	  * insert 插入元素
+	  * 
+	  * **/
+	 public void insert( int key){
+		 TreeNode parentNode = null;
+		 TreeNode newNode = new TreeNode(key,null,null,null);
+		 TreeNode pNode = root;
+		 if(root == null){
+			 root = newNode;
+			 return;
+		 }
+		 while(pNode != null){
+			 parentNode = pNode;
+			 if(key < pNode.key)
+			 {
+				 pNode = pNode.leftChild;
+			 }
+			 else if (key > pNode.key){
+				 pNode = pNode.rightChild;
+			 }
+			 else {
+				 return;//若有重复，则直接返回
+			 }
+		 }
+		 newNode.parent = parentNode;
+		 if(newNode.key < parentNode.key){
+			 parentNode.leftChild = newNode;
+		 }
+		 else{
+			 parentNode.rightChild = newNode;
+		 }
+		 
+		 
+	 }
+	 
+	 /**
+	  * transplant 用一个结点changeNode代替另一个结点originNode的位置
+	  * 
+	  * **/
+	 public void transplant(TreeNode originNode, TreeNode changeNode){
+		 if(originNode.parent == null){
+			 this.root = changeNode;
+		 }
+		 else if (originNode == originNode.parent.leftChild){
+			 originNode.parent.leftChild = changeNode;
+		 }
+		 else {
+			 originNode.parent.rightChild = changeNode;
+		 }
+		 if(changeNode != null){
+			 changeNode.parent = originNode.parent;
+		 }
+	 }
+	 
+	 /**
+	  * deleteNode 删除一个结点，《算法导论》，用到辅助transplant
+	  * 
+	  * **/
+	 public void deleteNode (TreeNode dNode) throws Exception{
+		 if(dNode.leftChild == null){
+			 transplant(dNode,dNode.rightChild);//若左子树为空，用右孩子代替
+		 }
+		 else if(dNode.rightChild == null){
+			 transplant(dNode, dNode.leftChild);//若右子树为空，用左孩子代替
+		 }
+		 else{													//若左右均存在，用右子树的最小节点rightMinNode	 															
+			 TreeNode rightMinNode = minNode(dNode.rightChild);//代替右孩子（先让 rightMinNode与其右孩子交换）
+			 if(rightMinNode.parent != dNode){
+				 transplant(rightMinNode,rightMinNode.rightChild);
+				 rightMinNode.rightChild = dNode.rightChild;
+				 dNode.rightChild.parent = rightMinNode;
+				 
+			 }
+			transplant(dNode,rightMinNode);
+			rightMinNode.leftChild = dNode.leftChild;
+			rightMinNode.leftChild.parent = rightMinNode;
+			 }
+		 }
+	 public void deleteKey(int key) throws Exception{
+		 deleteNode(search(root, key));
+	 }
+		 
+	 
 
+	 public static void main (String[] args) throws Exception{
+		 BianrySearchTree bst = new BianrySearchTree();
+		 int[] keys = new int []{15, 6, 18, 3, 7, 13, 20, 2, 9, 4};
+		 for (int key: keys) {
+			 
+			bst.insert(key);
+		}
+		 bst.inOrderTraverse(bst.root);
+		 bst.deleteKey(18);
+		 bst.inOrderTraverse(bst.root);
+		 
+		 
+	
+	
+}
 }
  
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,5 +1,43 @@
 package sort;
 
 public class QuickSort {
+	public static int [] S = {13,19,9,5,12,8,7,4,21,2,6,11};
+	public static int partition(int[] S, int p, int r){
+		int i = p-1;
+		int j;
+		int x = S[r];
+		for ( j = p; j < r; j++) {
+			if(S[j] <= x){
+				i++;
+				int temp;
+				temp = S[j];  //½»»»
+				S[j] = S[i];
+				S[i] = temp;
+				}
+		}
+		int temp;
+		temp = S[r];
+		S[r] = S[i+1];
+		S[i+1] = temp;
+		return i+1;				
+	}
+	public static void quickSort(int[] S, int p, int r){
+		int q;
+		if(p < r){
+			q = partition(S, p, r);
+			quickSort(S, p, q-1);
+			quickSort(S, q+1, r); 
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		quickSort(S, 0, S.length-1);
+		for (int i = 0; i < S.length; i++) {
+			System.out.println(S[i]);
+		}
+		
+
+	}
 
 }
